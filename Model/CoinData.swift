7709429c1741e0c2805cc49c016965921314c204
Sami Cheema <102137871+SamiCh246@users.noreply.github.com>
2023-07-15 21:30:12 -0400

    Add files via upload

---
+++ Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Sami Sajjad Cheema on 7/6/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
